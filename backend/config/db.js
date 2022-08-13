2e0d335ed33fa7cc4b5444b0eb9eae5c95628c13
Daniel <daniel-mcintyre@hotmail.com>
2022-08-13 11:09:17 -0400

    backend setup and creation of folder structure

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+require('dotev').config;
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    } catch (error) {
+
+    }
+}
